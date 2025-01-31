09deec05c2c4f333158fbf38033662a2e328903a
Riza <104048502+Rizahilmanfirmansyah@users.noreply.github.com>
2025-02-01 06:12:48 +0700

    Update Form1.vb

--- Project_Inventory_VB/Form1.vb
+++ Project_Inventory_VB/Form1.vb
@@ -1,13 +1,25 @@
-﻿Public Class Form1
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Timer1.Start()
     End Sub
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
 
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Percentagelbl.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        MyprogressBar.Increment(1)
+        Percentagelbl.Text = MyprogressBar.Value
+        If MyprogressBar.Value = 100 Then
+            Me.Hide()
+            Dim log = New Login
+            log.Show()
+            Timer1.Enabled = False
+        End If
 
     End Sub
 End Class

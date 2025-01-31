ed9af3cef347bd0a421bec3d21afb9fa7157c431
Riza <104048502+Rizahilmanfirmansyah@users.noreply.github.com>
2025-02-01 06:13:21 +0700

    Update Home.vb

--- Project_Inventory_VB/Home.vb
+++ Project_Inventory_VB/Home.vb
@@ -1,5 +1,45 @@
-﻿Public Class Home
+Public Class Home
     Private Sub Home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Hide()
+        Dim prod = New Product
+        prod.Show()
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        Me.Hide()
+        Dim users = New Users
+        users.Show()
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Me.Hide()
+        Dim cate = New Category
+        cate.Show()
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Me.Hide()
+        Dim cust = New Customer
+        cust.Show()
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+        Me.Hide()
+        Dim orders = New Order
+        orders.Show()
+    End Sub
+
+    Private Sub Logout_Click(sender As Object, e As EventArgs) Handles Logout.Click
+        Me.Hide()
+        Dim log = New Login
+        log.Show()
     End Sub
 End Class
